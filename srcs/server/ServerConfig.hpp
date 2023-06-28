4ea050a2660da6722d9f7b54b7f64af191a8ebde
Pedro Paulo <pe.pn03@gmail.com>
2023-06-27 23:12:58 -0300

    Add redirect and update CGI configuration

--- srcs/server/ServerConfig.hpp
+++ srcs/server/ServerConfig.hpp
@@ -21,25 +21,27 @@ class ServerConfig {
 		const int							getBodySizeLimit(void) const;
 		const std::string					&getRoot(void) const;
 		const std::string					&getIndex(void) const;
-		const std::string					&getCgi(void) const;
+		const std::vector<std::string>		&getCgi(void) const;
 		const std::set<std::string>			&getServerNames(void) const;
 		const std::map<int, std::string>	&getErrorPages(void) const;
 		const std::set<std::string>			&getHttpMethods(void) const;
 		const std::string					&getUploadPath(void) const;
+		const std::string					getRedirect(void) const;
 		const bool							getDirList(void) const;
 
 	private:
 
-		int									_port;
-		int									_bodySizeLimit;
-		std::string							_root;
-		std::string							_index;
-		std::string							_cgi;
-		std::set<std::string>				_serverNames;
-		std::map<int, std::string>			_errorPages;
-		std::set<std::string>				_httpMethods;
-		std::string							_uploadPath;
-		bool								_dirList;
+		int							_port;
+		int							_bodySizeLimit;
+		std::string					_root;
+		std::string					_index;
+		std::vector<std::string>	_cgi;
+		std::set<std::string>		_serverNames;
+		std::map<int, std::string>	_errorPages;
+		std::set<std::string>		_httpMethods;
+		std::string					_uploadPath;
+		std::string					_redirect;
+		bool						_dirList;
 
 };
 
