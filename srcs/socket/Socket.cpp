48b9ad51ba05c5d4dacbbf74bb04831d712b160c
Pedro Paulo <pe.pn03@gmail.com>
2023-07-03 20:29:13 -0300

    Use getaddrlen to create sockets and make addresses reusable

--- srcs/socket/Socket.cpp
+++ srcs/socket/Socket.cpp
@@ -8,25 +8,15 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-Socket::Socket() {
-	this->_address.sin_family = AF_INET;
-	this->_address.sin_addr.s_addr = INADDR_ANY;
-	this->_address.sin_port = htons(0);
-	memset(this->_address.sin_zero, '\0', sizeof(this->_address.sin_zero));
-	this->_addrlen = sizeof(this->_address);
-	this->_socketaddr = reinterpret_cast<struct sockaddr *>(&this->_address);
-	this->_socketAddrlen = reinterpret_cast<socklen_t *>(&this->_addrlen);
-	this->_socketfd = -1;
-}
-
-Socket::Socket(int port, int backlog) : _port(port), _backlog(backlog) { 
-	this->_address.sin_family = AF_INET;
-	this->_address.sin_addr.s_addr = INADDR_ANY;
-	this->_address.sin_port = htons(this->_port);
-	memset(this->_address.sin_zero, '\0', sizeof(this->_address.sin_zero));
-	this->_addrlen = sizeof(this->_address);
-	this->_socketaddr = reinterpret_cast<struct sockaddr *>(&this->_address);
-	this->_socketAddrlen = reinterpret_cast<socklen_t *>(&this->_addrlen);
+Socket::Socket(int backlog, std::string const &port) : _backlog(backlog), _port(port) {
+	_optval = 1;
+	memset(&_hints, 0, sizeof(_hints));
+	_hints.ai_family = AF_UNSPEC;
+	_hints.ai_socktype = SOCK_STREAM;
+	_hints.ai_flags = AI_PASSIVE;
+	if (getaddrinfo(NULL, _port.c_str(), &_hints, &_serverInfo) != 0)
+		throw std::runtime_error("Failed to get socket info (getaddrinfo)");
+	_addrSize = sizeof(this->_clientAddr);
 	this->_socketfd = -1;
 }
 
@@ -37,22 +27,25 @@ Socket::Socket(Socket const &rhs) {
 Socket&	Socket::operator=(Socket const &rhs) {
 	if (&rhs == this)
 		return (*this);
-	this->_address = rhs._address;
-	this->_addrlen = rhs._addrlen;
-	*this->_socketAddrlen = *rhs._socketAddrlen;
-	*this->_socketaddr = *rhs._socketaddr;
 	this->_backlog = rhs._backlog;
 	this->_port = rhs._port;
 	this->_socketfd = rhs._socketfd;
+	this->_hints = rhs._hints;
+	this->_addrSize = rhs._addrSize;
+	this->_clientAddr = rhs._clientAddr;
+	this->_optval = rhs._optval;
 	return (*this);
 }
 
 Socket::~Socket(void) {
+	freeaddrinfo(_serverInfo);
 }
 
 void	Socket::initialize(void) {
-	if ((this->_socketfd = socket(AF_INET, SOCK_STREAM, 0)) == 0) // Estes parametros serão provavelmente variáveis
+	if ((this->_socketfd = socket(_serverInfo->ai_family, _serverInfo->ai_socktype, _serverInfo->ai_protocol)) == 0)
 		throw std::runtime_error("When creating socket");
+	if (setsockopt(this->_socketfd, SOL_SOCKET, SO_REUSEADDR, &_optval, sizeof(_optval)) == -1)
+		throw std::runtime_error("Fail to set socket options (setsockopt)");
 	if (this->_bindSocket() < 0)
 		throw std::runtime_error("When binding socket");
 	if (_listen(this->_backlog) < 0)
@@ -60,7 +53,7 @@ void	Socket::initialize(void) {
 }
 
 int	Socket::_bindSocket(void) {
-	return (bind(this->_socketfd, this->_socketaddr, this->_addrlen));
+	return (bind(this->_socketfd, _serverInfo->ai_addr, _serverInfo->ai_addrlen));
 }
 
 int	Socket::_listen(int backlog) {
@@ -70,7 +63,7 @@ int	Socket::_listen(int backlog) {
 int	Socket::acceptConnection(void) {
 	int				newSocket;
 
-	if ((newSocket = accept(this->_socketfd, this->_socketaddr, this->_socketAddrlen)) < 0)
+	if ((newSocket = accept(this->_socketfd, (struct sockaddr *) &this->_clientAddr, &_addrSize)) < 0)
 		throw std::runtime_error("In accepting connection");
 	return (newSocket);
 }
