12dd4e2c0321f8d5b5571077538a0025b950534f
wportilh <willianportilho01@gmail.com>
2023-09-05 19:49:54 -0300

    add test.py post test

---
+++ cgi_bin/test.py
@@ -0,0 +1,27 @@
+import os
+
+if "Body-Path" in os.environ and os.environ["Body-Path"] != "":
+    file_path = os.environ["Body-Path"]
+    with open(file_path, "r") as file:
+        text = file.read()
+else:
+    text = ""
+
+htmlContent = """<!DOCTYPE html>
+<html>
+<head>
+	<title>200 OK</title>
+</head>
+<body>
+	<h1>Successful request</h1>
+	<p>{}</p>
+</body>
+</html>\r\n\r\n""".format(text)
+
+contentLength = len(htmlContent)
+
+response = """HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n
+
+{}""".format(contentLength, htmlContent)
+
+print(response)
