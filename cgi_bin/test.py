a44771d9e80f813060c7668c725856cf2c92acfe
wportilh <willianportilho01@gmail.com>
2023-09-05 20:01:30 -0300

    add Content-type protection (test.py script)

--- cgi_bin/test.py
+++ cgi_bin/test.py
@@ -1,9 +1,10 @@
 import os
 
 if "Body-Path" in os.environ and os.environ["Body-Path"] != "":
-    file_path = os.environ["Body-Path"]
-    with open(file_path, "r") as file:
-        text = file.read()
+	if "Content-Type" in os.environ and os.environ["Content-Type"] == "text/plain":
+		file_path = os.environ["Body-Path"]
+		with open(file_path, "r") as file:
+			text = file.read()
 else:
     text = ""
 
